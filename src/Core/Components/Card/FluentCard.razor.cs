90495dbe724e49b3e8502213cb861f0bb30580f9
magniveo <michailanatger@mail.ru>
2024-08-14 17:46:28 +0500

    g-card

--- src/Core/Components/Card/FluentCard.razor.cs
+++ src/Core/Components/Card/FluentCard.razor.cs
@@ -6,15 +6,49 @@ namespace Microsoft.FluentUI.AspNetCore.Components;
 public partial class FluentCard
 {
     protected string? StyleValue => new StyleBuilder(Style)
-        .AddStyle("--card-width", Width, !string.IsNullOrEmpty(Width))
-        .AddStyle("--card-height", Height, !string.IsNullOrEmpty(Height))
+        .AddStyle("width", Width, !string.IsNullOrEmpty(Width))
+        .AddStyle("height", Height, !string.IsNullOrEmpty(Height))
         .AddStyle("content-visibility", "visible", !AreaRestricted)
         .AddStyle("contain", "none", !AreaRestricted)
         .Build();
-
+/*
+ * width: '120px';
+   height: '120px';
+   display: 'flex';
+   alignItems: 'center';
+   justifyContent: 'center';
+ */
     protected string? ClassValue => new CssBuilder(Class)
-        .AddClass("fluent-card-minimal-style", when: MinimalStyle)
+        .AddClass("g-card")
+        .AddClass("g-card_view_outlined")
+        .AddClass(GetCssClassTheme())
+        .AddClass("g-box")
+        .AddClass("g-card_type_container")
+        .AddClass(GetCssClassSize())
         .Build();
+    [Parameter]
+    public string Theme { get; set; } = "normal";
+    [Parameter]
+    public string Size { get; set; } = "m";
+
+    private string GetCssClassSize()
+    {
+        return $"g-card_size_{Size}";
+    }
+
+    private string GetCssClassTheme()
+    {
+        return $"g-card_theme_{Theme}";
+        switch (Theme)
+        {
+            case "normal" :return  "g-card_theme_normal";
+            case "info" :return  "g-card_theme_info";
+            case "success" :return  "g-card_theme_success";
+            case "warning" :return  "g-card_theme_warning";
+            case "danger" :return  "g-card_theme_danger";
+        }
+        return  "g-card_theme_normal";
+    }
 
     /// <summary>
     /// By default, content in the card is restricted to the area of the card itself.
